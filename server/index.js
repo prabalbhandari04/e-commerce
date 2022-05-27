51bb0ab5cee44ede4a536aea408eb158610e612c
Prabal Bhandari <prabalb046348@gmail.com>
2022-05-27 08:58:54 +0545

    mongo atlas genesis

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
-
+const mongoose = require('mongoose')
 
 const app = express()
 app.use(express.json())
@@ -9,6 +9,15 @@ app.use(cors())
 app.use(cookieParser())
 
 
+// Connect to mongodb
+
+mongoose.connect("mongodb+srv://viron:GqZkjxd283@cluster0.7p7fp.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err => {
+    if(err) throw err;
+    console.log("Connected to mongodb")
+})
 
 
 const PORT = process.env.PORT || 5000
