f057a7aff6554a8796839d8c0b808a61bf617e68
Prabal Bhandari <prabalb046348@gmail.com>
2022-05-27 09:12:38 +0545

    middleware cleand for now and function validate email added

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,7 +1,5 @@
 const router = require('express').Router()
 const userController = require('../controllers/userController')
-const auth = require('../middleware/auth')
-const authAdmin = require('../middleware/authAdmin')
 
 router.post('/register', userController.register)
 
